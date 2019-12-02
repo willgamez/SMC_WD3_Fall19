a0f06d49b7e8013a4ad735d1fd1545bf598b592d
willgamez <willgamez@gmail.com>
2019-12-01 23:54:03 -0800

    site architecture created
    Built basic site architecture off of existing website I had built, working through trying to figure out where items will be placed, and what store will look like. updated nav links, updated animated icon for drop down menu. updated landing page with some images to serve as placeholders. Worked on trying to implement a cart. Failed miserably. scrapped it. will attempt again tomorrow.

---
+++ final/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#myModal").modal('show');
+});
+
+$('.alert').alert()
+
+$(document).ready(function() {
+    $(".navbar-toggler").click(function() {
+      $(".navbar-toggler-icon").toggleClass("active");
+    });
+  });
+
+  
